a597d0e5fb1d02a3548a8a5d5fc3b4f7db81238e
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2024-08-05 15:23:02 +0200

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,39 +1,37 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { 
-  getAuth, 
-  initializeAuth, 
-  getReactNativePersistence, 
-  setPersistence,
-  browserLocalPersistence
+import {
+  getFirestore, doc, getDoc, setDoc, updateDoc, deleteDoc, addDoc, collection, getDocs, query, where
+} from 'firebase/firestore';
+import {
+  getAuth, initializeAuth, setPersistence, browserLocalPersistence, GoogleAuthProvider, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, sendPasswordResetEmail, fetchSignInMethodsForEmail, sendEmailVerification
 } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Platform } from "react-native";
+import { getReactNativePersistence } from "firebase/auth/react-native";
 
-export { collection, doc, getDoc, setDoc, getDocs, query, updateDoc, deleteDoc, addDoc, where, onSnapshot } from "firebase/firestore";
-export { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail, fetchSignInMethodsForEmail, sendEmailVerification } from "firebase/auth";
-
+// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_PROJECT_ID + ".firebaseapp.com",
+  authDomain: `${process.env.EXPO_PUBLIC_PROJECT_ID}.firebaseapp.com`,
   projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_PROJECT_ID + ".appspot.com",
+  storageBucket: `${process.env.EXPO_PUBLIC_PROJECT_ID}.appspot.com`,
 };
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-export const db = getFirestore();
+export const db = getFirestore(firebaseApp);
 export const storage = getStorage(firebaseApp);
 
-//Check platform for login persistence
+// Initialize Auth based on platform
 const isWeb = Platform.OS === 'web';
-export const auth = isWeb ? getAuth(firebaseApp)
+export const auth = isWeb
+  ? getAuth(firebaseApp)
   : initializeAuth(firebaseApp, {
-      persistence: getReactNativePersistence(AsyncStorage),
+    persistence: getReactNativePersistence(AsyncStorage),
   });
 
-//Extra check for web
+// For web, set persistence
 if (isWeb) {
   setPersistence(auth, browserLocalPersistence)
     .catch((error) => {
@@ -41,4 +39,7 @@ if (isWeb) {
     });
 }
 
-
+export {
+  doc, getDoc, setDoc, updateDoc, deleteDoc, addDoc, collection, getDocs, query, where,
+  getAuth, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, GoogleAuthProvider, sendPasswordResetEmail, fetchSignInMethodsForEmail, sendEmailVerification,
+};
