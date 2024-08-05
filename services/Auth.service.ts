31225dbef7984739a2e1f98067bb4cd316f289a4
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2024-08-05 15:21:52 +0200

    Update Auth.service.ts

--- services/Auth.service.ts
+++ services/Auth.service.ts
@@ -1,86 +1,56 @@
 import * as Google from 'expo-auth-session/providers/google';
-import { doc, updateDoc } from "firebase/firestore";
-import { 
-    auth, 
-    getAuth,
-    signInWithEmailAndPassword, 
-    createUserWithEmailAndPassword, 
-    signOut, GoogleAuthProvider, 
-    signInWithPopup, 
-    sendPasswordResetEmail, 
-    fetchSignInMethodsForEmail, 
-    sendEmailVerification, 
-    firebaseApp
-} from "./firebase";
-
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithCredential, sendPasswordResetEmail, fetchSignInMethodsForEmail, sendEmailVerification } from "firebase/auth";
 import { createUser, getUserById } from "./User.service";
 import { userData } from "../common/types/User";
-import { signInWithCredential } from "firebase/auth";
+import { firebaseApp } from "./firebase";
 
-export const signUp = async (username: string , name:string, lastName: string, email: string, password: string) => {
-    try {
-        const userCredential = await createUserWithEmailAndPassword(
-            auth, 
-            email,
-            password
-            );
-        sendEmailVerification(userCredential.user);
+// Sign up function
+export const signUp = async (username: string, name: string, lastName: string, email: string, password: string) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(getAuth(firebaseApp), email, password);
+    await sendEmailVerification(userCredential.user);
 
-        //Create user
-        const user = {
-            uid: userCredential.user.uid,
-            email: userCredential.user.email,
-            name: name,
-            lastName: lastName,
-            username: username,
-            date: new Date(),
-        } as userData;
-        console.log(user);
+    // Create user data
+    const user: userData = {
+      uid: userCredential.user.uid,
+      email: userCredential.user.email,
+      name: name,
+      lastName: lastName,
+      username: username,
+      date: new Date(),
+      profileImage: null,
+    };
+    console.log(user);
 
-        const userCreatedId = await createUser(user);
-        console.log(userCreatedId);
+    const userCreatedId = await createUser(user);
+    console.log(userCreatedId);
 
-        return userCreatedId;
-    } catch (error) {
-        if (error instanceof Error) {
-            return error.message;
-        } else {
-            return "An unexpected error occurred";
-        }
+    return userCreatedId;
+  } catch (error) {
+    if (error instanceof Error) {
+      return error.message;
+    } else {
+      return "An unexpected error occurred";
     }
-}
-
-export const logIn = async (email: string, password: string) => {
-    const auth = getAuth(firebaseApp);
-    try {
-        await signInWithEmailAndPassword(
-            auth, 
-            email, 
-            password
-        );
-        //Maybe check getUserbyid
-        console.log("User logged in (logIn function)", auth.currentUser?.uid);
-        const user = await getUserById(auth.currentUser?.uid ?? '') as userData;
-        return user;
-    } catch (error) {
-        console.log(error)
-        return false;
-    }
+  }
 };
 
-// export const checkEmailAndPass = async (email, password) => {
-//     const r = await fetchSignInMethodsForEmail(auth, email);
-//     if (r.includes("google.com")) {
-//         return "google.com";
-//     }
-//     try {
-//         const result = await signInWithEmailAndPassword(auth, email, password);
-//         return result.user.uid;
-//     } catch (err) {
-//         return err.message;
-//     }
-// };
+// Log in function
+export const logIn = async (email: string, password: string) => {
+  const auth = getAuth(firebaseApp);
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    console.log("User logged in (logIn function)", auth.currentUser?.uid);
+    const user = await getUserById(auth.currentUser?.uid ?? '') as userData;
+    console.log("User logged in (handleLogin)", user);
+    return user;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
 
+// Log in with Google function
 export const logInWithGoogle = async () => {
   try {
     const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
@@ -90,7 +60,7 @@ export const logInWithGoogle = async () => {
     if (response?.type === 'success') {
       const { idToken } = response.params;
       const credential = GoogleAuthProvider.credential(idToken);
-      const auth = getAuth();
+      const auth = getAuth(firebaseApp);
       const userCredential = await signInWithCredential(auth, credential);
       const user = userCredential.user;
       console.log("User logged in with Google:", user);
@@ -104,6 +74,7 @@ export const logInWithGoogle = async () => {
           name: user.displayName,
           lastName: null,
           date: new Date(),
+          profileImage: null,
         };
         console.log(newUser);
 
@@ -121,31 +92,9 @@ export const logInWithGoogle = async () => {
   }
 };
 
-// export const EditProfile = async (uid, name, email,date) => {
-//     await updateDoc(doc(db, "users", uid), {
-//         id: uid,
-//         email: email,
-//         name: name,
-//         date: date,
-//     });
-// };
-
+// Log out function
 export const logout = async () => {
-    await signOut(auth);
-    console.log("User logged out");
+  await signOut(getAuth(firebaseApp));
+  console.log("User logged out");
 };
 
-//recover password
-// export const sendPass = (email) => {
-//     sendPasswordResetEmail(auth, email)
-//         .then(() => {
-//             return "Email sent";
-//             // Password reset email sent!
-//             // ..
-//         })
-//         .catch((error) => {
-//             const errorCode = error.code;
-//             const errorMessage = error.message;
-//             // ..
-//         });
-// };
