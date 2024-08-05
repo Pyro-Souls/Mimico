b86412303035402d57286e201a4d8024aff1c382
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2024-08-05 15:29:22 +0200

    Update User.ts

--- common/types/User.ts
+++ common/types/User.ts
@@ -1,8 +1,10 @@
 export type userData = {
-    uid: string | null;  
+    uid: string | null;
     username: string | null;
     email: string | null;
     name: string | null;
     lastName: string | null;
     date: Date | null;
+    profileImage: string | null;
 };
+
