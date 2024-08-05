4912fa78843db811b43aa73d1e4d2ad3e91f6a0e
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2024-08-05 15:28:43 +0200

    Update store.ts
    Summary of Changes:
    Added Characters Interface:
    
    New interface Characters defined to represent character data with id, title, subtitle, and stats.
    Updated AppState Interface:
    
    Added data state to store a list of characters.
    Added setData action to update the data state.
    Added currentCharacter state to track the currently selected character.
    Added setCurrentCharacter action to set the current character.
    Added loadUserCharacters action to asynchronously load characters based on the user's ID.
    Updated Initial State:
    
    Added profileImage field to the user state.
    Added Logic for Loading Characters:
    
    Implemented loadUserCharacters function to fetch and set user-specific character data using getUserCharacters.

--- providers/store.ts
+++ providers/store.ts
@@ -1,14 +1,32 @@
 import { create } from 'zustand';
 import { userData } from '../common/types/User';
+import { getUserCharacters } from '../services/User.service';
 
+// Define the new 'Characters' interface
+interface Characters {
+    id: string;
+    title: string;
+    subtitle: string;
+    stats: {
+        strength: number;
+        agility: number;
+    };
+}
 
-type State = {
+// Update the AppState interface to include new state and actions
+interface AppState {
     user: userData;
     setUser: (user: userData) => void;
     clearUser: () => void;
-};
+    data: Characters[]; // Added state to store character data
+    setData: (data: Characters[]) => void; // Added action to set character data
+    currentCharacter: Characters | null; // Added state to store the currently selected character
+    setCurrentCharacter: (character: Characters | null) => void; // Added action to set the current character
+    loadUserCharacters: () => Promise<void>; // Added asynchronous action to load user characters
+}
 
-const useStore = create<State>((set) => ({
+const useStore = create<AppState>((set, get) => ({
+    // Updated 'user' state to include 'profileImage'
     user: {
         uid: null,
         username: null,
@@ -16,6 +34,7 @@ const useStore = create<State>((set) => ({
         name: null,
         lastName: null,
         date: null,
+        profileImage: null, // New field added to user data
     },
     setUser: (user) => set({ user }),
     clearUser: () => set({
@@ -26,8 +45,20 @@ const useStore = create<State>((set) => ({
             name: null,
             lastName: null,
             date: null,
-        }
+            profileImage: null, // New field added to user data
+        },
     }),
+    data: [], // Added initial state for character data
+    setData: (data) => set({ data }), // Added action to update character data
+    currentCharacter: null, // Added initial state for the current character
+    setCurrentCharacter: (character) => set({ currentCharacter: character }), // Added action to set the current character
+    loadUserCharacters: async () => { // Added action to load characters based on user ID
+        const user = get().user;
+        if (user.uid) {
+            const characters = await getUserCharacters(user.uid);
+            set({ data: characters });
+        }
+    },
 }));
 
 export default useStore;
